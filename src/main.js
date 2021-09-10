b9a068b8ba7c8e975c925fefbf502bc323661b32
raloliver <contato@raloliver.com.br>
2021-09-10 07:47:19 +0100

    fixed lint

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
  * File: main.js
  * Project: xchange-converter
  * Created: Friday, September 10th 2021, 6:04:05 am
- * Last Modified: Friday, September 10th 2021, 7:44:01 am
+ * Last Modified: Friday, September 10th 2021, 7:46:52 am
  * Copyright © 2021 AMDE Agência
  */
 
@@ -14,6 +14,6 @@ import {version} from '../package.json';
 const program = commander;
 
 program
-  .version(version)
-  .description('A CLI to convert EUR to any currency provided.')
-  .parse(process.argv);
+    .version(version)
+    .description('A CLI to convert EUR to any currency provided.')
+    .parse(process.argv);
