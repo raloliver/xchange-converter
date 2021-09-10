2dd72b0820149f037ba9b34d3f02d329e80f1d7e
raloliver <contato@raloliver.com.br>
2021-09-10 06:46:11 +0100

    just for now, disable console.log warning on eslint

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@
  * File: main.js
  * Project: xchange-converter
  * Created: Friday, September 10th 2021, 6:04:05 am
- * Last Modified: Friday, September 10th 2021, 6:39:15 am
+ * Last Modified: Friday, September 10th 2021, 6:45:45 am
  * Copyright © 2021 AMDE Agência
  */
 
+// eslint-disable-next-line no-console
 console.log('Hello CLI');
