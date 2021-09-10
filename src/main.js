c6a4db64234c8e85a1d14708344dadf68ff886c8
raloliver <contato@raloliver.com.br>
2021-09-10 06:42:38 +0100

    init cli

---
+++ src/main.js
@@ -0,0 +1,9 @@
+/*
+ * File: main.js
+ * Project: xchange-converter
+ * Created: Friday, September 10th 2021, 6:04:05 am
+ * Last Modified: Friday, September 10th 2021, 6:39:15 am
+ * Copyright © 2021 AMDE Agência
+ */
+
+console.log('Hello CLI');
---
+++ tests/main.spec.js
@@ -0,0 +1,27 @@
+/*
+ * File: main.spec.js
+ * Project: xchange-converter
+ * Created: Friday, September 10th 2021, 6:04:05 am
+ * Last Modified: Friday, September 10th 2021, 6:42:01 am
+ * Copyright © 2021 AMDE Agência
+ */
+
+import chai, {expect} from 'chai';
+import * as child from 'child_process';
+
+const exec = child.exec;
+const xConverter = 'node.exe ./src/main.js';
+
+/**
+ * using local var (done) to finalize the async method
+ * it is necessary to replace \n because this is default behavior of console.log
+ */
+describe('Main CLI', () => {
+  it('should return something', (done) => {
+    exec(xConverter, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal('Hello CLI');
+      done();
+    });
+  });
+});
